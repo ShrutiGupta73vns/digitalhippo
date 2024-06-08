e0db4d639d26616f3bb041b5a8f1994ffc47330f
Shruti Gupta <shrurose73@gmail.com>
2024-06-08 16:29:32 +0530

    database modelling

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,26 +4,33 @@ import { Button, buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Bluetooth } from "lucide-react";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
-import {zodResolver} from '@hookform/resolvers/zod'
-import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  AuthCredentialsValidator,
+  TAuthCredentialsValidator,
+} from "@/lib/validators/account-credentials-validator";
 import { trpc } from "@/trpc/client";
-import {toast} from 'sonner'
+import { toast } from "sonner";
 import { ZodError } from "zod";
 
 import { useRouter, useSearchParams } from "next/navigation";
 
 const Page = () => {
-  
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const isSeller = searchParams.get("as") === "seller";
+  const origin = searchParams.get("origin");
 
-    const searchParams =useSearchParams()
-    const router =useRouter()
-    const isSeller = searchParams.get('as') ==='seller'
-    const origin = searchParams.get('origin')
+  const continueAsSeller = () => {
+    router.push("?as=seller");
+  };
 
-    
+  const continueAsBuyer = () => {
+    router.replace("/sign-in", undefined);
+  };
 
   const {
     register,
@@ -33,35 +40,27 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   });
 
-  
-const {mutate,isLoading}= trpc.auth.signIn.useMutation({
-onSuccess:()=>{
-  toast.success("Signed in successfully")
-router.refresh()
+  const { mutate: signIn, isLoading } = trpc.auth.signIn.useMutation({
+    onSuccess: () => {
+      toast.success("Signed in successfully");
+      router.refresh();
 
-if(origin){
-  router.push(`${origin}`)
-  return
-}
+      if (origin) {
+        router.push(`${origin}`);
+        return;
+      }
 
-if(isSeller){
- router.push('/sell')
-return
-}
-router.push('/')
-
-},
-onError:(err)=>{
-  
-}
-
-
-})
-const onSubmit=({email,password}: TAuthCredentialsValidator)=>{
-       mutate({email,password})
-}
-
-
+      if (isSeller) {
+        router.push("/sell");
+        return;
+      }
+      router.push("/");
+    },
+    onError: (err) => {},
+  });
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
+    signIn({ email, password });
+  };
 
   return (
     <>
@@ -69,7 +68,7 @@ const onSubmit=({email,password}: TAuthCredentialsValidator)=>{
         <div className="mx-auto flex flex-col w-full justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2 text-center">
             <Icons.logo className="h-20 w-20" />
-            <h1 className="text-2xl font-bold ">Sign in to your account</h1>
+            <h1 className="text-2xl font-bold ">Sign in to your {isSeller? 'seller' : ''}{' '} account</h1>
             <Link
               className={buttonVariants({
                 variant: "link",
@@ -78,7 +77,7 @@ const onSubmit=({email,password}: TAuthCredentialsValidator)=>{
               href="/sign-up"
             >
               {" "}
-              don&apos;t have an account? 
+              don&apos;t have an account?
               <ArrowRight className="h-4 w-4" />
             </Link>
           </div>
@@ -88,45 +87,57 @@ const onSubmit=({email,password}: TAuthCredentialsValidator)=>{
                 <div className="grid gap-2 py-2">
                   <Label htmlFor="email">Email</Label>
                   <Input
-                  {...register('email')}
+                    {...register("email")}
                     className={cn({
                       "focus-visible:ring-red-500": errors.email,
                     })}
                     placeholder="you@example.com"
                   />
                   {errors?.email && (
-                    <p className="text-sm text-red-500">{errors.email.message}</p>
+                    <p className="text-sm text-red-500">
+                      {errors.email.message}
+                    </p>
                   )}
                 </div>
                 <div className="grid gap-2 py-2">
                   <Label htmlFor="password">Password</Label>
                   <Input
-                  {...register('password')}
-                  type='password'
+                    {...register("password")}
+                    type="password"
                     className={cn({
                       "focus-visible:ring-red-500": errors.password,
                     })}
                     placeholder="password "
                   />
-                    {errors?.password && (
-                    <p className="text-sm text-red-500">{errors.password.message}</p>
+                  {errors?.password && (
+                    <p className="text-sm text-red-500">
+                      {errors.password.message}
+                    </p>
                   )}
                 </div>
                 <Button>Sign in</Button>
-
-
               </div>
             </form>
             <div className="relative">
-                <div aria-hidden='true' className="absolute inset-0 flex items-center">
-                    <span className="w-full border-t"/>
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                    <span className="bg-background px-2 text-muted-foreground">
-                        or
-                    </span>
-                </div>
+              <div
+                aria-hidden="true"
+                className="absolute inset-0 flex items-center"
+              >
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  or
+                </span>
+              </div>
             </div>
+            {isSeller ? (
+              <Button onClick={continueAsBuyer} variant="secondary" disabled={isLoading}>
+                Continue as Customer
+              </Button>
+            ) : (
+              <Button onClick={continueAsSeller} variant="secondary" disabled={isLoading}>Continue as seller</Button>
+            )}
           </div>
         </div>
       </div>
