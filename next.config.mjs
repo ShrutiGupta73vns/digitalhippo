e8d9f1026c7303f4a2320f1c1712833b38e19f4c
Shruti Gupta <shrurose73@gmail.com>
2024-09-01 19:53:51 +0530

    debugging

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "digitalhippo-production-54a3.up.railway.app",
-			},
+        domains: [
+			'localhost',
+            'digitalhippo-production-54a3.up.railway.app',
 		],
     }
 };
